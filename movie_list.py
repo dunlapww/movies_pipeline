493b2be7ffed2e952a1341401b5b0f1a6d5814da
Will Dunlap <dunlapww@gmail.com>
2021-03-23 13:14:02 -0600

    add import_movies method to movie_list.py

--- movie_list.py
+++ movie_list.py
@@ -1,3 +1,12 @@
+import pandas as pd
+
 class MovieList:
   def __init__(self,csv_path):
-    self.csv = csv_path
+    self.movies = self.import_movies(csv_path)
+  
+  def import_movies(self, csv_path):
+    col_list = ["id", "genres", "release_date"]
+    header_list = ['genres', 'movie_id', 'release_date']
+    df = pd.read_csv(csv_path, usecols = col_list)
+    df.columns = header_list
+    return df
