c5409a5adb7bb6574e56586be85f16b84985deda
Will Dunlap <dunlapww@gmail.com>
2021-03-23 16:07:42 -0600

    add test for bad genres

--- test_movie_list.py
+++ test_movie_list.py
@@ -1,5 +1,6 @@
 #import pdb; pdb.set_trace()
 
+import math
 import pandas as pd
 from movie_list import MovieList
 
@@ -36,3 +37,66 @@ def test_it_can_add_year_column():
   assert m1.movies.columns[3] == 'release_year'
   assert len(str(m1.movies.values[0][3])) == 4
   assert type(m1.movies.values[0][3]) == int
+
+def test_it_can_flag_duplicate_records():
+  csv_path = 'movies_metadata_sample.csv'
+  m1 = MovieList(csv_path)
+  test_data = [{'movie_id': 1}, {'movie_id': 1}]
+  m1.movies = pd.DataFrame(test_data)
+  m1.add_mark_dupes()
+  assert m1.movies.columns[1] == 'dupes'
+  assert m1.movies.values[0][1] == False
+  assert m1.movies.values[1][1] == True
+
+def test_it_can_check_id_is_digit():
+  csv_path = 'movies_metadata_sample.csv'
+  m1 = MovieList(csv_path)
+  test_data = [{'movie_id': 1}, {'movie_id': 'a'}]
+  m1.movies = pd.DataFrame(test_data)
+  m1.add_id_is_digit()
+  assert m1.movies.columns[1] == 'id_is_digit'
+  assert m1.movies.values[0][1] == True
+  assert m1.movies.values[1][1] == False
+
+def test_it_can_validate_a_list():
+  csv_path = 'movies_metadata_sample.csv'
+  m1 = MovieList(csv_path)
+  test_dict1 = {'name':'adventure'}
+  test_dict2 = {'name':'crime'}
+  test_list1 = f'[{test_dict1}, {test_dict2}]'
+  test_list2 = f'[{test_dict1}, {test_dict2}'
+  test_list3 = f'[]'
+  test_list4 = ""
+  assert m1.list_check(test_list1) == True
+  assert m1.list_check(test_list2) == False
+  assert m1.list_check(test_list3) == False
+  assert m1.list_check(test_list4) == False
+
+def test_it_can_convert_to_list():
+  csv_path = 'movies_metadata_sample.csv'
+  m1 = MovieList(csv_path)
+  test_dict1 = {'name':'adventure'}
+  test_dict2 = {'name':'crime'}
+  test_list1 = f'[{test_dict1}, {test_dict2}]'
+  test_list2 = f'[{test_dict1}, {test_dict2}'
+  test_list3 = f'[]'
+  assert len(m1.to_list(test_list1)) == 2
+  assert len(m1.to_list(test_list2)) == 0
+  assert len(m1.to_list(test_list3)) == 0
+
+def test_it_can_add_genre_column():
+  csv_path = 'movies_metadata_bad_genre.csv'
+  m1 = MovieList(csv_path)
+  test_dict1 = {'name':'adventure'}
+  test_dict2 = {'name':'crime'}
+  test_list1 = f'[{test_dict1}, {test_dict2}]'
+  m1.add_genre_list()
+  assert m1.movies.columns[3] == 'genre_list'
+  assert len(m1.movies.values[0][3]) > 0
+  assert len(m1.movies.values[1][3]) == 0
+  assert len(m1.movies.values[2][3]) > 0
+  assert len(m1.movies.values[3][3]) == 0
+  assert len(m1.movies.values[4][3]) > 0
+  assert len(m1.movies.values[5][3]) == 0
+  
+  
