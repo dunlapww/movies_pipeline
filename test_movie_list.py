8bb72612d82d175fead680bc3691b1d30f64d63e
Will Dunlap <dunlapww@gmail.com>
2021-03-23 13:13:38 -0600

    add test that dataframe columns are correct

--- test_movie_list.py
+++ test_movie_list.py
@@ -1,11 +1,17 @@
+#import pdb; pdb.set_trace()
+
+import pandas as pd
 from movie_list import MovieList
 
 def test_it_exists():
-  csv_path = "Will"
+  csv_path = "movies_metadata_sample.csv"
   m1 = MovieList(csv_path)
   assert type(m1) == MovieList
 
-def test_it_can_create_a_dataframe():
+def test_it_can_create_a_dataframe_of_movie_details():
   csv_path = 'movies_metadata_sample.csv'
   m1 = MovieList(csv_path)
-  assert m1.csv == 'movies_metadata_sample.csv'
+  assert type(m1.movies) == pd.DataFrame
+  assert m1.movies.columns[0] == 'genres'
+  assert m1.movies.columns[1] == 'movie_id'
+  assert m1.movies.columns[2] == 'release_date'
