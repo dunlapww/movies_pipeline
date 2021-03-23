fe6e00e62ae2844bd8dd78acfc194963aa62df23
Will Dunlap <dunlapww@gmail.com>
2021-03-23 13:27:07 -0600

    add test to add column

--- test_movie_list.py
+++ test_movie_list.py
@@ -15,3 +15,10 @@ def test_it_can_create_a_dataframe_of_movie_details():
   assert m1.movies.columns[0] == 'genres'
   assert m1.movies.columns[1] == 'movie_id'
   assert m1.movies.columns[2] == 'release_date'
+
+def test_it_can_add_an_error_column():
+  csv_path = 'movies_metadata_sample.csv'
+  m1 = MovieList(csv_path)
+  m1.add_movies_column('error_desc', 'no error')
+  assert m1.movies.columns[3] == 'error_desc'
+  assert m1.movies.values[0][3] == 'no error'
