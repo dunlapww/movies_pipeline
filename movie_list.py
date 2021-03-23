dffe4f1838cc613c5392cddf703b5e4dcd3621c2
Will Dunlap <dunlapww@gmail.com>
2021-03-23 16:07:52 -0600

    pass test for bad genres

--- movie_list.py
+++ movie_list.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import math
 class MovieList:
   def __init__(self,csv_path):
     self.movies = self.import_movies(csv_path)
@@ -23,4 +23,28 @@ class MovieList:
   def add_release_year(self):
     self.movies["release_year"] = self.movies['release_date'].apply(lambda x: self.release_year(x))
 
+  def add_mark_dupes(self):
+    self.movies['dupes'] = self.movies.duplicated(subset = 'movie_id', keep = 'first')
+
+  def add_id_is_digit(self):
+    self.movies['id_is_digit'] = self.movies['movie_id'].apply(lambda x: str(x).isdigit())
+
+  #this method reduces risk of using 'eval' in the to_list method
+  #by ensuring the string contents is generally of list structure
+  def list_check(self, data):
+    try:
+      if math.isnan(data): return False
+    except:
+      return len(data) > 2 and data[0] == '[' and data[-1] == ']'
   
+  def to_list(self, genres):
+    if self.list_check(genres) == False: 
+      return []
+    
+    if type(eval(genres)) == list:
+      return eval(genres)
+    else:
+      return []
+
+  def add_genre_list(self):
+    self.movies['genre_list'] = self.movies['genres'].apply(lambda x: self.to_list(x))
